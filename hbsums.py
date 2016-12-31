a852920b4c23e9baa7abf3a08b08fdd25b7f81dd
MR Wirth <dreikin@gmail.com>
2016-12-31 05:38:10 -0500

    Get the essential properties for each download.
    Properties are:
    
    * File name
    * MD5 checksum
    
    Still need to strip the url to just the file name.

--- hbsums.py
+++ hbsums.py
@@ -10,7 +10,11 @@ def main(argv=None):
     data = json.load(fp)
     products = data['subproducts']
     for product in products:
-        print(product['human_name'] + "\n")
+        for download in product['downloads']:
+            for ds in download['download_struct']:
+                filename = ds['url']['web']
+                checksum = ds['md5']
+                print(checksum + ' *./' + filename)
 
 if __name__ == "__main__":
     main(sys.argv)
