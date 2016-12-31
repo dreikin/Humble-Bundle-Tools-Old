a707250b62b95cd447a6bef3741816594c8f05b5
MR Wirth <dreikin@gmail.com>
2016-12-31 05:28:59 -0500

    Use dict access, not property access.  Use correct argv index.

--- hbsums.py
+++ hbsums.py
@@ -3,14 +3,14 @@ import json
 
 
 def main(argv=None):
-    if not argv:
+    if not argv[1]:
         return
 
-    fp = open(argv[0])
+    fp = open(argv[1])
     data = json.load(fp)
-    products = data.subproducts
+    products = data['subproducts']
     for product in products:
-        print(product.human_name + "\n")
+        print(product['human_name'] + "\n")
 
 if __name__ == "__main__":
     main(sys.argv)
